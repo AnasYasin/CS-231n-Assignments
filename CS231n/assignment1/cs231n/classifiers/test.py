ebf153131003a1b7d88e8b748911e63f499381b1
AnasYasin <34492965+AnasYasin@users.noreply.github.com>
2019-06-09 19:33:47 +0500

    Add files via upload

---
+++ CS231n/assignment1/cs231n/classifiers/test.py
@@ -0,0 +1,7 @@
+import numpy as np
+from itertools import chain
+k_choices = [1, 3, 5, 8, 10, 12, 15, 20, 50, 100]
+
+#for k in k_choices:
+for i in range(k_choices.__len__()):
+    print(k_choices[i])
